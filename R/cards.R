24cb865a22be170fa3de75e5783ca10bba04a5a7
Divad0175 <dgranjon@ymail.com>
2019-01-05 14:36:44 +0100

    add toolbox to cards

--- R/cards.R
+++ R/cards.R
@@ -8,6 +8,10 @@
 #' @param footer Card footer. NULL by default. Not displayed if NULL.
 #' @param status Card status. NULL by default. See \link{https://preview.tabler.io/docs/colors.html} for valid statuses.
 #' @param statusSide Status side: "top" or "left".
+#' @param collapsible Whether the card is collapsible. TRUE by default.
+#' @param collapsed Whether to collapse the card at start. FALSE by default.
+#' @param closable Whether the card is closable. TRUE by default.
+#' @param zoomable Whether the card is zoomable. TRUE by default.
 #'
 #' @examples
 #' if(interactive()){
@@ -36,7 +40,9 @@
 #'
 #' @export
 tablerCard <- function(..., title = NULL, options = NULL, footer = NULL,
-                       status = NULL, statusSide = c("top", "left")) {
+                       status = NULL, statusSide = c("top", "left"),
+                       collapsible = TRUE, collapsed = FALSE, closable = TRUE,
+                       zoomable = TRUE) {
 
   statusSide <- match.arg(statusSide)
 
@@ -47,20 +53,49 @@ tablerCard <- function(..., title = NULL, options = NULL, footer = NULL,
     }
   }
 
+  cardCl <- "card"
+  if (collapsed) cardCl <- paste0(cardCl, " card-collapsed")
+
   shiny::tags$div(
-    class = "card",
+    class = cardCl,
     if (!is.null(status)) shiny::tags$div(class = statusCl),
     # header
     if (!is.null(title)) {
       shiny::tags$div(
         class = "card-header",
         shiny::tags$h3(class = "card-title", title),
-        if (!is.null(options)) {
-          shiny::tags$div(
-            class = "card-options",
-            options
-          )
-        }
+
+        # card toolbox and other elements such as buttons, ...
+        shiny::tags$div(
+          class = "card-options",
+          if (collapsible) {
+            shiny::tags$a(
+              href = "#",
+              class = "card-options-collapse",
+              `data-toggle` = "card-collapse",
+              shiny::tags$i(class = "fe fe-chevron-up")
+            )
+          },
+          if (zoomable) {
+            if (closable) {
+              shiny::tags$a(
+                href = "#",
+                class = "card-options-fullscreen",
+                `data-toggle` = "card-fullscreen",
+                shiny::tags$i(class = "fe fe-maximize")
+              )
+            }
+          },
+          if (closable) {
+            shiny::tags$a(
+              href = "#",
+              class = "card-options-remove",
+              `data-toggle` = "card-remove",
+              shiny::tags$i(class = "fe fe-x")
+            )
+          },
+          if (!is.null(options)) options
+        )
       )
     },
     # body
