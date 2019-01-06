1d799a64a3a8089d3cfe5ce6f8cf6e02be40fad7
Divad0175 <dgranjon@ymail.com>
2019-01-06 22:21:52 +0100

    add blog card

--- R/cards.R
+++ R/cards.R
@@ -326,3 +326,103 @@ tablerMediaCard <- function(..., title = NULL, date = NULL, href = NULL,
   )
 
 }
+
+
+
+
+
+#' Create a Boostrap 4 blog card
+#'
+#' Build a tabler blog card
+#'
+#' @inheritParams tablerMediaCard
+#' @param author Blog post author.
+#' @param horizontal Whether the card is horizontally displayed. FALSE by default.
+#'
+#' @examples
+#' if(interactive()){
+#'  library(shiny)
+#'  library(tablerDash)
+#'
+#'  shiny::shinyApp(
+#'    ui = tablerDashPage(
+#'     navbar = NULL,
+#'     footer = NULL,
+#'     title = "test",
+#'     body = tablerDashBody(
+#'      tablerBlogCard(
+#'       title = "Blog Card",
+#'       author = "David",
+#'       date = "Today",
+#'       href = "https://www.google.com",
+#'       src = "https://preview.tabler.io/demo/photos/matt-barrett-339981-500.jpg",
+#'       avatarUrl = "https://image.flaticon.com/icons/svg/145/145842.svg",
+#'       width = 6,
+#'       "Look, my liege! The Knights Who Say Ni
+#'       demand a sacrifice! …Are you suggesting
+#'       that coconuts migr..."
+#'      )
+#'     )
+#'    ),
+#'    server = function(input, output) {}
+#'  )
+#' }
+#'
+#' @author David Granjon, \email{dgranjon@@ymail.com}
+#'
+#' @export
+tablerBlogCard <- function(..., title = NULL, author = NULL, date = NULL, href = NULL,
+                           src = NULL, avatarUrl = NULL, width = 4, horizontal = FALSE) {
+
+  cardCl <- "card"
+  if (horizontal) cardCl <- paste0(cardCl, " card-aside")
+
+  shiny::column(
+    width = width,
+    shiny::tags$div(
+      class = cardCl,
+      if (!is.null(src)) {
+        if (!horizontal) {
+          shiny::tags$a(
+            href = href,
+            target = "_blank",
+            shiny::tags$img(
+              class = "card-img-top",
+              src = src
+            )
+          )
+        } else {
+          shiny::tags$a(
+            href = href,
+            target = "_blank",
+            class = "card-aside-column",
+            style = paste0("background-image: url(\"", src, "\")")
+          )
+        }
+      },
+      # body
+      shiny::tags$div(
+        class = "card-body d-flex flex-column",
+        shiny::h4(shiny::a(href = "#", title)),
+        shiny::tags$div(class = "text-muted", ...),
+        shiny::tags$div(
+          class = "d-flex align-items-center pt-5 mt-auto",
+          shiny::tags$div(
+            class = "d-flex align-items-center px-2",
+            shiny::tagAppendAttributes(
+              tablerAvatar(
+                url = avatarUrl,
+                size = "md"
+              ),
+              class = "mr-3"
+            ),
+            shiny::tags$div(
+              shiny::tags$div(author),
+              htmltools::tags$small(class = "d-block text-muted", date)
+            )
+          )
+        )
+      )
+    )
+  )
+}
