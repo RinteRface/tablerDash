cf0e74b192b1062983fcd49efcb5a5f7eaa58de3
Divad0175 <dgranjon@ymail.com>
2019-02-05 14:56:08 +0100

    try to make it run on shinyapps.io (tabs issue)

--- inst/tablerDash-0.1.0/init.js
+++ inst/tablerDash-0.1.0/init.js
@@ -1,11 +1,25 @@
 $(function () {
-  //var $tablinks = $("#mymenu .nav-item a");
-  //console.log($tablinks);
 
   // select the first nav item by default at start
   $('#mymenu .nav-item:eq(0) a').addClass('active');
   $('#mymenu .nav-item:eq(0) a').tab('show');
   $('.container-fluid.tab-pane:eq(0)').addClass('active show');
+
+  // handles shinyapps.io
+  var workerId = $('base').attr('href');
+  // ensure that this code does not locally
+  if (typeof workerId != "undefined") {
+    // get the initial page url
+    var url = window.location.href;
+    // list all tabs
+    $("#mymenu .nav-item a").each(function(){
+      // get the tablink href value
+      var tablink = "/" + $(this).attr("href");
+      // set the new url taking worker id as reference
+      $(this).attr("href", url + workerId + tablink);
+      console.log($(this));
+    });
+  }
 
   // handle when the user click on other items
   //$('.nav-item').on('click', function (event) {
